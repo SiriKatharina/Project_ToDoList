52ec879f4dde375e334054f3471cda2c2b55cb3b
Irfan Mulani <irfan.mulani@assurant.com>
2022-04-13 12:31:03 +0530

    Unit test changes

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -4,7 +4,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { delay, of } from 'rxjs';
+import { delay, Observable, of } from 'rxjs';
 import { TodoListService } from '../service/todo-list.service';
 import { Todo } from '../service/todo';
 
@@ -15,7 +15,7 @@ describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
   // let todolistServiceSpy: jasmine.SpyObj<TodoListService>;
-  
+
   beforeEach(async () => {
     //  const todolistServiceSpyObj = jasmine.createSpyObj('TodoListService', ['getData']);
     await TestBed.configureTestingModule({
@@ -51,56 +51,71 @@ describe('TodoListComponent', () => {
     expect(component.todolist).toEqual([{ id: 1, name: 'Test', description: 'Test description', prio: 'high', status: 0 }]);
   }));
 
-  it('should call delete a todo', fakeAsync(() => {
+  // it('should call delete a todo', fakeAsync(() => {
+  //   const successResponse = { message: 'Deletion was successful' };
+  //   let todoListService = fixture.debugElement.injector.get(TodoListService);
+  //   let stub = spyOn(todoListService, 'deleteData').and.callFake(() => {
+  //     return of({ "message": "Deletion was successful" }).pipe(delay(300));
+  //   })
+
+
+  //   //expect(todoListService.deleteData).toHaveBeenCalled();
+  //   tick(400);
+  //   stub.calls.mostRecent().returnValue.subscribe({
+  //     next: (v) => {
+  //       expect(successResponse).toEqual({ "message": "Deletion was successful" });
+  //       expect(component.getPosts).toHaveBeenCalled();
+  //     }
+  //   });
+
+  it('should delte a todo', () => {
     let todoListService = fixture.debugElement.injector.get(TodoListService);
-    let stub = spyOn(todoListService, 'deleteData').and.callFake(() => {
-      return of({ "message": "Deletion was successful" }).pipe(delay(300));
-    })
+    spyOn(todoListService, 'deleteData').and.returnValue(of({ 'message': 'Deltetion was successful' }));
+    spyOn(component, 'getPosts');
+
     component.deleteTodo(1);
-    tick(300);
-    stub.calls.mostRecent().returnValue.subscribe({
-      next: (v) => {
-        component.getPosts();
-      }, error: (e) => console.error(e)
-    });
-    expect(component.todolist).toEqual([{"message": "Deletion was successful"}]);
-    //expect('').toEqual('');
-    // spyOn(component, 'getPosts').and.stub();
 
-  }));
+    expect(todoListService.deleteData).toHaveBeenCalled();
+    expect(component.getPosts).toHaveBeenCalled();
+  })
 
-  it('should call the delete method by click only once',() =>{
-    /*let todoListService = fixture.debugElement.injector.get(TodoListService);
-    let mockSpy = jasmine.createSpyObj(['deleteTodo']);
-    mockSpy.deleteTodo.and.returnValue(of(true));
-   //component.getPosts = ;
-    component.deleteTodo(1)
-    /*
-    spyOn(todoListService, 'deleteData').and.callFake(() => {
-      return of({ "message": "Deletion was successful" }).pipe(delay(300));
-    })
-    component.deleteTodo(1);
-    expect(mockSpy.deleteTodo).toHaveBeenCalledTimes(1);*/
-  });
+  //expect('').toEqual('');
+  // spyOn(component, 'getPosts').and.stub();
 
-  it('should add a new todo to the todo-List over a post', fakeAsync(() => {
-    let todoListService = fixture.debugElement.injector.get(TodoListService); // mock from the class TodoListService
-    let stub = spyOn(todoListService, 'postData').and.callFake(() => {
-      return of({"message": "Insertion was successful"}).pipe(delay(300));
-    })
-    stub.calls.mostRecent().returnValue.subscribe({
-      next: (v) => {
-        component.addTodo();
-      }, error: (e) => console.error(e)
-    });
-    //component.addTodo(); //--> call the Method add a todo
-    tick(300);
-    //fixture.detectChanges();
-    //const compiled = fixture.debugElement.nativeElement;
-   
-    //expect(compiled.innerHTML).toContain([{"message": "Insertion was successful"}]);
-    expect(component.addTodo).toEqual([{message: "Insertion was successful"}]); // what should I expected?
-  })); //--> equal to the String? possible? 
+
+
+  //  it('should call the delete method by click only once', () => {
+  /*let todoListService = fixture.debugElement.injector.get(TodoListService);
+  let mockSpy = jasmine.createSpyObj(['deleteTodo']);
+  mockSpy.deleteTodo.and.returnValue(of(true));
+ //component.getPosts = ;
+  component.deleteTodo(1)
+  /*
+  spyOn(todoListService, 'deleteData').and.callFake(() => {
+    return of({ "message": "Deletion was successful" }).pipe(delay(300));
+  })
+  component.deleteTodo(1);
+  expect(mockSpy.deleteTodo).toHaveBeenCalledTimes(1);*/
+  //});
+
+  // it('should add a new todo to the todo-List over a post', fakeAsync(() => {
+  //   let todoListService = fixture.debugElement.injector.get(TodoListService); // mock from the class TodoListService
+  //   let stub = spyOn(todoListService, 'postData').and.callFake(() => {
+  //     return of({ "message": "Insertion was successful" }).pipe(delay(300));
+  //   })
+  //   stub.calls.mostRecent().returnValue.subscribe({
+  //     next: (v) => {
+  //       component.addTodo();
+  //     }, error: (e) => console.error(e)
+  //   });
+  //   //component.addTodo(); //--> call the Method add a todo
+  //   tick(300);
+  //   //fixture.detectChanges();
+  //   //const compiled = fixture.debugElement.nativeElement;
+
+  //   //expect(compiled.innerHTML).toContain([{"message": "Insertion was successful"}]);
+  //   expect(component.addTodo).toEqual([{ message: "Insertion was successful" }]); // what should I expected?
+  // })); //--> equal to the String? possible? 
 
 
   it('should RESET Form', () => {
@@ -111,7 +126,7 @@ describe('TodoListComponent', () => {
   });
 
   it('edit todo', () => {
-    let todo =  {id: 1, name:"test", description: "testD", prio:"1", date: "01.03.22", status: 1};
+    let todo = { id: 1, name: "test", description: "testD", prio: "1", date: "01.03.22", status: 1 };
     component.editTodo(todo);
     expect(component.todoName).toEqual('test');
     expect(component.todoDescription).toEqual('testD');
@@ -121,25 +136,25 @@ describe('TodoListComponent', () => {
     expect(component.updateMode).toEqual(true);
   });
 
-  it('update todo, should save the new data in the database from the updated todo',  fakeAsync(() => {
-    let todoListService = fixture.debugElement.injector.get(TodoListService);
-    let stub = spyOn(todoListService, 'postData').and.callFake(() => {
-      return of({"message": "Update was successful"}).pipe(delay(300));
-    })
-    let todo =  {id: 1, name:"test", description: "testD", prio:"1", date: 2022-22-10, status: 0};
-    component.updateTodo(todo); //--> call the Method add a todo
-    tick(300);
-    stub.calls.mostRecent().returnValue.subscribe({
-      next: (v) => {
-        component.updateTodo(todo);
-      }, error: (e) => console.error(e)
-    });
-    expect(component.todolist).toEqual([{"message": "Update was successful"}]);
-  }));
+  // it('update todo, should save the new data in the database from the updated todo', fakeAsync(() => {
+  //   let todoListService = fixture.debugElement.injector.get(TodoListService);
+  //   let stub = spyOn(todoListService, 'postData').and.callFake(() => {
+  //     return of({ "message": "Update was successful" }).pipe(delay(300));
+  //   })
+  //   let todo = { id: 1, name: "test", description: "testD", prio: "1", date: 2022 - 22 - 10, status: 0 };
+  //   component.updateTodo(todo); //--> call the Method add a todo
+  //   tick(300);
+  //   stub.calls.mostRecent().returnValue.subscribe({
+  //     next: (v) => {
+  //       component.updateTodo(todo);
+  //     }, error: (e) => console.error(e)
+  //   });
+  //   expect(component.todolist).toEqual([{ "message": "Update was successful" }]);
+  // }));
 
-  
+
   it('should toggle the Status in a todo', () => {
-    let todo =  {id: 1, status: 1};
+    let todo = { id: 1, status: 1 };
     component.toggleStatus(todo);
     expect(component.showDone).toEqual(false);
   });
